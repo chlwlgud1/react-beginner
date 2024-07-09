184457a9fe15f1c71052f144b3394e4932bfdcff
chlwlgud1 <44596476+chlwlgud1@users.noreply.github.com>
2024-07-09 13:53:49 +0900

    Create App2.js

---
+++ App2.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selCoins, setSelCoins] = useState("0");
+  const [result, setResult] = useState("0");
+
+  const onChange = (event) => {
+    setSelCoins(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const priceUSD = event.target[0].value;
+    calculate(priceUSD);
+  };
+
+  const calculate = (priceUSD) => {
+    setResult(priceUSD * selCoins);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading....</strong>
+      ) : (
+        <select onChange={onChange}>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} : {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      <form onSubmit={onSubmit}>
+        <input type="number" placeholder="write us dollars" />
+        <button>change coins</button>
+      </form>
+      <h2>you can change {result} coins!</h2>
+    </div>
+  );
+}
+
+export default App;
